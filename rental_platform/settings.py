6fffd03b1c82d9a0028ef9c94ee4202f0e306ff8
Georgii Chetverikov <georgiichetverikov@mail.ru>
2025-12-23 17:24:07 +0300

    Update settings.py

--- rental_platform/settings.py
+++ rental_platform/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from .config import SECRET_KEY, DEBUG, DATABASES
 import os
+import os
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-!mv1$(1ynsh*ao^#!1k477u5#o82m(#l1hj4wprw*nk2u3&25-'
@@ -47,14 +50,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rental_platform.wsgi.application'
 
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rental_db',
-        'USER': 'postgres',
-        'PASSWORD': '1235789Cg',  # ← убрали пробел
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME', 'rental_db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1235789Cg'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 LOGOUT_REDIRECT_URL = '/accounts/login/'
